36fe6224d8e81be78c0b817b4028fb78a9342916
brn <brn@neusta.de>
2023-02-24 10:09:34 +0100

    Test für die Map geändert

--- src/main/java/com/example/demo/Map.java
+++ src/main/java/com/example/demo/Map.java
@@ -3,8 +3,10 @@ package com.example.demo;
 public class Map {
     String[][] map;
     public void createField(int xCoordinate, int yCoordinate) {
+        this.map = new String[xCoordinate][yCoordinate];
     }
 
     public String[][] getMap() {
+        return this.map;
     }
 }
