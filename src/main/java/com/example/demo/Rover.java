825f3e38812c6099d97ab046ff2fb2c4dc995ea7
Fynn Kretzner <fynn.kretzner@encoway.de>
2023-03-10 10:25:03 +0100

    Implemented Rover

--- src/main/java/com/example/demo/Rover.java
+++ src/main/java/com/example/demo/Rover.java
@@ -5,18 +5,61 @@ public class Rover {
     private int x;
     private int y;
     private char direction;
-    public Rover(int x, int y,char direction){
+
+    public Rover(int x, int y, char direction) {
         this.x = x;
         this.y = y;
         this.direction = direction;
-    };
+    }
+
+    public void moveRover(String movement) {
+        if (movement.equals("f")) {
+            switch (direction) {
+                case 'S' -> setY(-1);
+                case 'N' -> setY(+1);
+                case 'E' -> setX(+1);
+                case 'W' -> setX(-1);
+            }
+        }
+        if (movement.equals("b")) {
+            switch (direction) {
+                case 'S' -> setY(+1);
+                case 'N' -> setY(-1);
+                case 'E' -> setX(-1);
+                case 'W' -> setX(+1);
+            }
+        }
+
+    }
+
+    public void turnRover(String turnDirection) {
+        if (turnDirection.equals("l")) {
+            switch (direction) {
+                case 'N' -> setDirection('W');
+                case 'W' -> setDirection('S');
+                case 'S' -> setDirection('E');
+                case 'E' -> setDirection('N');
+            }
+
+        }
+        if (turnDirection.equals("r")) {
+            switch (direction) {
+                case 'N' -> setDirection('E');
+                case 'W' -> setDirection('N');
+                case 'S' -> setDirection('W');
+                case 'E' -> setDirection('S');
+            }
+
+        }
+
+    }
 
     public int getX() {
         return x;
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.x += x;
     }
 
     public int getY() {
@@ -24,7 +67,7 @@ public class Rover {
     }
 
     public void setY(int y) {
-        this.y = y;
+        this.y += y;
     }
 
     public char getDirection() {
