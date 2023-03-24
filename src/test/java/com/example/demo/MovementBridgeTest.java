e6e4abe5982458036cdea5c6f7932a53ae86f031
Fynn Kretzner <fynn.kretzner@encoway.de>
2023-03-24 10:50:06 +0100

    Created MovementBridgeTest

---
+++ src/test/java/com/example/demo/MovementBridgeTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class MovementBridgeTest {
+
+    @Test
+    void ValidateMovementOrdersTest(){
+        MovementBridge movementBridge = new MovementBridge();
+        assertEquals("fflff",movementBridge.validateMovementOrders("fAf@lff"));
+
+    }
+
+    @Test
+    void ProcessMovementOrdersTest(){
+        MovementBridge movementBridge = new MovementBridge();
+        assertEquals("f", movementBridge.processMovementOrders("fflff").get(0));
+        assertEquals("l", movementBridge.processMovementOrders("fflff").get(2));
+
+    }
+}
